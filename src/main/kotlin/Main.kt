1b6e53cdefb53c6c835dd94aca77213190a74243
Гавриш Евгений <ntx5@yandex.ru>
2022-03-07 19:56:32 +0300

    Исправлена 1 и 2 задачи

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -14,12 +14,17 @@ fun checkCommission() {
         result = 3500.0
     }
     println("Комиссия: " + Math.round(result) + " коп.")
-
 }
 
-fun checkLikes() {
-    val likes = 211
-    val result = if (likes % 2 == 0) " людям" else " человеку"
+fun checkLikes(){
+    val likes = 2
+    var result = " людям"
+    val value = likes % 100
+    val num = value % 10
+    if(value > 10 && value < 20) result = " людям"
+    if(num > 1 && num < 5) result = " людям"
+    if(num == 1) result = " человеку"
+
     println("Понравилось: " + likes + result)
 }
 
